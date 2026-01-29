c1f8a980c4610d39900f2ff196fe64e0eb8afa83
ARVIND <arvind124gangwar@gmail.com>
2026-01-29 21:18:18 +0530

    node run start 3

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const router = require('./routers/user.router');
 app.use('/api/users', router);
 app.use('/api/products', productRouter);
 const port = process.env.port ? process.env.port : 3000;
-
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
